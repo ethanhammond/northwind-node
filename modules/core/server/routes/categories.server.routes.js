c689bcb479038623ff6cab1935950c2cd74aa801
Ethan Hammond <ethanhammond12099@gmail.com>
2016-12-01 11:14:55 -0500

    Fixed json id issue with categories

--- modules/core/server/routes/categories.server.routes.js
+++ modules/core/server/routes/categories.server.routes.js
@@ -16,9 +16,6 @@ module.exports = function(app) {
 		.put(categories.update)
 		.delete(categories.delete);
 
-	// Finish by binding the article middleware
-	// What's this? Where the categoryId is present in the URL
 	// the logic to 'get by id' is handled by this single function
-	// and added to the request object i.e. request.category.
 	app.param('categoryId', categories.categoryByID);
 };
